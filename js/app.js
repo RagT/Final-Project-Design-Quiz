8ba53e2c8409c6a52f5a534613d232c643f424c6
RagT <raghutir@gmail.com>
2015-07-17 16:23:14 -0700

    Addition of routes  and templates

---
+++ js/app.js
@@ -0,0 +1,34 @@
+angular.module('designApp', ['ngRoute'])
+
+.config(function($routeProvider){
+	$routeProvider
+	.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'quizCtrl',
+	})
+	.when('/1/', {
+		templateUrl: 'templates/part1.html',
+		controller: 'quizCtrl',
+
+	})
+	.when('/2/', {
+		templateUrl: 'templates/part2.html',
+		controller: 'quizCtrl',
+	})
+	.when('/3/', {
+		templateUrl: 'templates/part3.html',
+		controller: 'quizCtrl',
+	})
+	.when('/4/', {
+		templateUrl: 'templates/part4.html',
+		controller: 'quizCtrl',
+	})
+	.when('/5/', {
+		templateUrl: 'templates/part5.html',
+		controller: 'quizCtrl',
+	})
+})
+
+.controller('quizCtrl', function($scope){
+
+})
